d0a67f4bbd6fa66fa0392d7e88ac544bc266b357
mfelipegs <matheusg.dasilva7@gmail.com>
2025-02-15 09:24:13 -0300

    chore: install validator and add dto

--- src/pancs/dto/create-panc.dto.ts
+++ src/pancs/dto/create-panc.dto.ts
@@ -1,1 +1,25 @@
-export class CreatePancDto {}
+import { IsString, IsArray, IsNotEmpty } from 'class-validator';
+
+export class CreatePancDto {
+  @IsString()
+  @IsNotEmpty()
+  namePanc: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsArray()
+  cultivation: string[];
+
+  @IsString()
+  @IsNotEmpty()
+  benefits: string;
+
+  @IsString()
+  image: string;
+
+  @IsString()
+  @IsNotEmpty()
+  locale: string;
+}
