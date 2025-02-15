80fc83c81c4c603d750cc4927228b4bcfa2ae8e5
mfelipegs <matheusg.dasilva7@gmail.com>
2025-02-15 11:03:57 -0300

    feature: recipes resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PancsModule } from './pancs/pancs.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
   imports: [
     PancsModule,
     MongooseModule.forRoot('mongodb://localhost:27017/osiris-nest'),
+    RecipesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
