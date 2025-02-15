a8904f2df261c2a8aa471a0ef0a4eea26bd5cc65
mfelipegs <matheusg.dasilva7@gmail.com>
2025-02-15 14:10:08 -0300

    chore: 204 code to delete

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { RecipesService } from './recipes.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
@@ -36,6 +37,7 @@ export class RecipesController {
   }
 
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id: string) {
     return this.recipesService.remove(id);
   }
