952ae08bf636b00936945f4df79d55615107aebe
mfelipegs <matheusg.dasilva7@gmail.com>
2025-02-15 18:11:11 -0300

    chore: protecting all pancs and recipes controllers

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -7,36 +7,43 @@ import {
   Param,
   Delete,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { RecipesService } from './recipes.service';
 import { CreateRecipeDto } from './dto/create-recipe.dto';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('recipes')
 export class RecipesController {
   constructor(private readonly recipesService: RecipesService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() createRecipeDto: CreateRecipeDto) {
     return this.recipesService.create(createRecipeDto);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.recipesService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.recipesService.findOne(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
     return this.recipesService.update(id, updateRecipeDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   @HttpCode(204)
   remove(@Param('id') id: string) {
     return this.recipesService.remove(id);
