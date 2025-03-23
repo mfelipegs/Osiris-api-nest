0f9b924aa658bf7b84b85ad26dbf722853a3d863
mfelipegs <matheusg.dasilva7@gmail.com>
2025-03-22 22:51:13 -0300

    feat: dto and schema prediction

--- src/prediction/dto/create-prediction.dto.ts
+++ src/prediction/dto/create-prediction.dto.ts
@@ -1,1 +1,33 @@
-export class CreatePredictionDto {}
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsMongoId,
+  IsEnum,
+  IsDate,
+} from 'class-validator';
+import { Types } from 'mongoose';
+
+export enum PredictionStatus {
+  PENDING = 'pending',
+  PROCESSED = 'processed',
+  FAILED = 'failed',
+}
+
+export class CreatePredictionDto {
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+
+  @IsDate()
+  @IsOptional()
+  date?: Date;
+
+  @IsEnum(PredictionStatus)
+  @IsNotEmpty()
+  status: PredictionStatus;
+
+  @IsMongoId()
+  @IsNotEmpty()
+  userId: Types.ObjectId;
+}
---
+++ src/prediction/schemas/prediction.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema()
+export class Prediction extends Document {
+  @Prop({ required: true })
+  image: string;
+
+  @Prop({ type: Date, default: () => new Date() })
+  date: Date;
+
+  @Prop({ required: true })
+  status: string;
+
+  @Prop({ required: true, type: Types.ObjectId, ref: 'User' })
+  userId: Types.ObjectId;
+}
+
+export const PredictionSchema = SchemaFactory.createForClass(Prediction);
